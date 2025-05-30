b17c8a4fb2c05faac1cc1c99df6ba2949ea63b75
Tatakarthesha <141987823+Tatakarthesha@users.noreply.github.com>
2025-05-30 19:59:27 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('https://shopping-backend-ybni.onrender.com')
+    fetch('https://shopping-backend-ybni.onrender.com/api/shopping')
       .then(res => res.json())
       .then(data => {
         setProducts(data.shopping);
